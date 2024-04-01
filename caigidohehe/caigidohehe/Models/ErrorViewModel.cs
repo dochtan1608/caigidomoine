744b2fa2ea17444bbbf37a43e8c1a736d1cddf8c
dochtan1608 <89788071+dochtan1608@users.noreply.github.com>
2024-04-01 16:12:27 +0700

    oke

---
+++ caigidohehe/caigidohehe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace caigidohehe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
